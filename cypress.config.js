5e32fd8479567803cfbbe025d9c714aae9d7ef0e
Jozek Hajduk <hajduksanchez.dev@gmail.com>
2022-06-23 12:12:58 -0500

    ✅ [ADD] Headless execution mode

--- cypress.config.js
+++ cypress.config.js
@@ -14,7 +14,7 @@ module.exports = defineConfig({
 			})
 		},
 		// Exclude specific test files
-		excludeSpecPattern: ['/cypress/e2e/1-getting-started/*.js', '/cypress/e2e/2-advanced-examples/*.js'],
+		excludeSpecPattern: ['**/e2e/1-getting-started/*', '**/e2e/2-advanced-examples/*'],
 		baseUrl: 'https://demoqa.com', // URL base of the website
 	},
 })
